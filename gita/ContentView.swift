a840ccbadd4d0a424187a9d53459595ee10b723e
vachanbhogi <vachanbhogi@outlook.com>
2026-06-15 05:41:38 -0700

    Project Init

---
+++ gita/ContentView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+@main struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
